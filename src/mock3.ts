63094c5370ff9134c4d8f4838cbd3b8e36c8f179
Daenam Kim <daenam.kim@gmail.com>
2023-03-07 14:28:11 +0900

    Fix a deprecated

--- src/mock3.ts
+++ src/mock3.ts
@@ -1,5 +1,4 @@
 import { ethers } from 'ethers';
-import { isNullOrUndefined } from 'util';
 
 interface SignerCache {
   [key: string]: ethers.Wallet;
@@ -38,7 +37,7 @@ class Mock3 {
   async listAccounts(): Promise<string[]> {
     const accounts: string[] = Object.values(this.signers).map(signer => signer.address)
 
-    if (!isNullOrUndefined(this.accountIndex)) {
+    if (this.accountIndex !== null && this.accountIndex !== undefined) {
       const found = accounts[this.accountIndex];
       return found ? Promise.resolve([found]) : Promise.resolve([]);
     }
